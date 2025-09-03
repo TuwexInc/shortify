6ff026b63054381bd6ad0c52e5dd8c9fc82de5f6
abelfleitas <afleitasp@gmail.com>
2025-09-03 02:33:21 -0400

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -42,7 +42,7 @@ export default defineConfig({
           },
           routes: {
             extend: {
-              exclude: [
+                include: [
                 { pattern: "/api/*" },
                 { pattern: "/s/*" }
               ]
